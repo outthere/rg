8a501b8de534d7690b2464873cdb0e12495a5924
Viktor Melnikov <heaven.out.there@gmail.com>
2011-12-12 17:46:41 +0200

    12.12.2011-4

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -3,6 +3,28 @@ class PartnersController < ApplicationController
   before_filter :find_all_partners
   before_filter :find_page
 
+  def become_partner_thank_you
+    @page = Page.find_by_link_url("/partners/become_partner_thank_you", :include => [:parts, :slugs])
+  end
+
+  def new
+    @partner = Partner.new
+  end
+
+  def create
+    @partner = Partner.new(params[:enrollment])
+
+#    puts params[:partner]
+
+    if @partner.save
+      redirect_to become_partner_thank_you_partners_url
+    else
+      flash[:notice] = "Some error"
+      render :action => 'new'
+    end
+  end
+
+
   def index
     # you can use meta fields from your model instead (e.g. browser_title)
     # by swapping @page for @partner in the line below:
